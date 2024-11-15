5f1245a170accf0376e1d84fdc9ac92652b69dc3
emess2g <essuonemma84gmail.com>
2024-11-15 21:16:26 +0000

    func play,pause

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useRef } from "react";
+import { createContext, useRef, useState } from "react";
+import { songsData } from "../assets/assets";
 
 export const PlayerContext =  createContext();
 
@@ -7,13 +8,39 @@ const PlayerContextProvider = (props) => {
      
     const audioRef = useRef();
     const seekBg = useRef();
-    const seekBar = useRef()
+    const seekBar = useRef();
 
+    const [track,setTrack] = useState(songsData[0]);
+    const [playStatus, setPlayStatus] = useState(false);
+    const [time,setTime] = useState({
+        currentTime:{
+            second:0,
+            minute:0
+        },
+        totalTime:{
+            second:0,
+            minute:0
+        }
+    })
+
+    const play = () => {
+        audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    const pause = () => {
+        audioRef.current.pause();
+        setPlayStatus(false)
+    }
 
     const contextValue = {
         audioRef,
         seekBar,
-        seekBg
+        seekBg,
+        track,setTrack,
+        time,setTime,
+        playStatus,setPlayStatus,
+        play,pause
     }
 
     return(
