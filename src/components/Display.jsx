20e12348d82c7ebb122d6a95303095173b6bcc10
emess2g <essuonemma84gmail.com>
2024-11-15 06:57:41 +0000

    displayhome comp

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Routes , Route} from 'react-router-dom'
+import DisplayHome from './DisplayHome'
 
 const Display = () => {
   return (
     <div className='w-full m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       <Routes>
-        <Route path='/' element={<DisplayHOme/>}/>
+        <Route path='/' element={<DisplayHome/>}/>
       </Routes>
     </div>
   )
