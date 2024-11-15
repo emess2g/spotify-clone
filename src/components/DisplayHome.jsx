aa258791808014e3be0e0b4ec3c47a832169afd7
emess2g <essuonemma84gmail.com>
2024-11-15 10:29:11 +0000

    todays pick mark up

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { albumsData } from '../assets/assets'
+import { albumsData, songsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
 
 const DisplayHome = () => {
@@ -11,6 +11,12 @@ const DisplayHome = () => {
         <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
         <div className="flex overflow-auto">
         {albumsData.map((item,index) => (<AlbumItem key={index} name={item.name} image={item.image} desc={item.desc} id={item.id}/>))} 
+        </div>
+      </div>
+      <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'>Today's Pick</h1>
+        <div className="flex overflow-auto">
+        {songsData.map} 
         </div>
       </div>
     </>
