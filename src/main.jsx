be9ab7814fac22a933de94081a2e51a2b0af8f60
emess2g <essuonemma84gmail.com>
2024-11-15 13:13:22 +0000

    fixed bug

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import { PlayerContext } from './context/PlayerContext.jsx'
+import PlayerContextProvider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <PlayerContext>
+    <PlayerContextProvider>
       <App />
-    </PlayerContext>
+    </PlayerContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
