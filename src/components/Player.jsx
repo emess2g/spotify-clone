8bf34dd52a30fc3c2555fb4d212bb0b4d067e744
emess2g <essuonemma84gmail.com>
2024-11-16 00:32:23 +0000

    time track

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -31,7 +31,7 @@ const Player = () => {
             <div ref={seekBg} className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
                 <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full' />
             </div>
-            <p>{time.currentTime.minute}:{time.currentTime.second}</p>
+            <p>{time.totalTime.minute}:{time.totalTime.second}</p>
         </div>
       </div>
       <div className="hidden lg:flex items-center gap-2 opacity-75">
